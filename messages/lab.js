49500fddb840b795305f8c0d2e86674f382f6ebb
Elias Marcopoulos <emarcopoulos@gmail.com>
2016-03-08 21:39:57 -0500

    parse function complete

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,22 @@
 // Your JavaScript goes here...
+function parse() {
+	console.log("HELLO");
+	request = new XMLHttpRequest();
+	console.log("REQUEST");
+	request.open("GET", "data.json", true);
+	console.log("OPEN");
+	request.send(null);
+	console.log("SEND");
+	request.onreadystatechange = function () {
+		if (request.status == 200 && request.readyState == 4) {
+			data = JSON.parse(request.responseText);
+			console.log(data);	
+			for (i = 0; i < data.length; i++) {
+				messages = document.getElementById("messages");
+				messages.innerHTML  = messages.innerHTML + "<p><span class = 'message'>" 
+									+ data[i].content + "</span><span class = 'name'>	" 
+									+ data[i].username + "</span></p>";
+			}
+		}
+	}
+}
